82108c2fea138552184ab2f485adb40dd51f5798
Sebastian <sebastianschapfel105@gmail.com>
2024-03-06 07:59:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const express = require("express")
 const https = require("https")
 const app = express()
 
-app.get("/chess/*", (req, res, next) => {
-  const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[2]
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
 app.get("/*", (req, res, next) => {
   //const pathSegments = req.path.split('/');
  // const rocketPath = pathSegments[2]
-  https.request(new URL("https://math.international/" + req.path), (resp) => {
+  https.request(new URL("https://shellshock.io/" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
