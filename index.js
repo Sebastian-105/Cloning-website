a6473833f1449d5713a81a4118523ac288b62b93
Sebastian <sebastianschapfel105@gmail.com>
2024-03-14 13:55:29 +0000

    m

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const https = require("https")
 const app = express()
+const port = 3000
 
 app.get("/*", (req, res, next) => {
-  //const pathSegments = req.path.split('/');
- // const rocketPath = pathSegments[2]
+const pathSegments = req.path.split('/');
+ const rocketPath = pathSegments[2]
   https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
 })
-app.listen(1050)
+console.log("this should work")
+console.log(`Running on port ${port}`)
+app.listen(port)
